46f3911ea163acb200b0e83f820203c986b53d5d
Ria <ekontou@ufl.edu>
2019-07-24 18:31:25 -0400

    install.R file required for Binder interactive env and requirements_R text for packages

---
+++ install.R
@@ -0,0 +1,9 @@
+install.packages('caret', dependencies = TRUE)
+install.packages('randomForest')
+install.packages('ggplot2')
+install.packages('ggfortify')
+install.packages('dplyr')
+install.packages('RSQLite')
+install.packages('DBI')
+install.packages('class')
+				
