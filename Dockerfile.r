cbd8670db151f45f9415713d68ccdeddb61ce8c6
nvk747 <nvk747@gmail.com>
2019-07-25 18:20:59 -0400

    final changes

--- Dockerfile.r
+++ Dockerfile.r
@@ -1,11 +1,3 @@
-From python:2.7.16
-
-#WORKDIR /usr/src/app
-
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
-CMD wget https://www.hydroshare.org/resource/9e1b23607ac240588ba50d6b5b9a49b5/data/contents/hampt_rd_data.sqlite && cd group6/db_scripts/ && python ./make_dly_obs_table.py && python ./prepare_flood_events_table.py  && python ./by_event_for_model.py
-
 From r-base:3.5.1
 RUN R -e "install.packages('caret',dependencies=TRUE,repos='http://cran.rstudio.com/')"
 RUN R -e "install.packages('ggfortify',dependencies=TRUE,repos='http://cran.rstudio.com/')"
